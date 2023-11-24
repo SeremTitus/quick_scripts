8e92e974ec73fd46a71cecdf5db7cddb2d6276b8
Serem Titus(SeremTitus) <seremtitusdev@gmail.com>
2023-11-25 02:07:27 +0300

    ORIGIN

---
+++ extend_git/get-branch.py
@@ -0,0 +1,16 @@
+import sys
+import os
+import subprocess
+
+def get_branch(branch_name):
+    try:
+        subprocess.check_output(['git', 'checkout','-b', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
+        subprocess.check_output(['git', 'pull', '--force', 'origin', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
+        print(f"Git get_branch and checkout {source}")
+    except subprocess.CalledProcessError as e:
+        print(f"Git get_branch and checkout ERROR : {e.output.splitlines()[0]}")
+
+if len(sys.argv) < 1:
+    print("add branch_name as arg")
+else:
+    get_branch(sys.argv[1])
