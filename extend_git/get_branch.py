b2e4738fafda97a47bd48bdb8bce9601e57dcdca
Serem Titus(SeremTitus) <seremtitusdev@gmail.com>
2023-11-25 05:47:22 +0300

    immprove

--- extend_git/get_branch.py
+++ extend_git/get_branch.py
@@ -7,12 +7,14 @@ def get_branch(branch_name):
         try:
             subprocess.check_output(['git', 'checkout','-b', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
         except subprocess.CalledProcessError as e:
-            print(f"Branch may exist ERROR : {e.output.splitlines()[0]}")
+            print(f"ERROR : {e.output.splitlines()[0]}")
             accept = input("branch will be rewriten,Y or YES to Continue anyway: ")
-            if not accept == "Y" or not accept == "YES":
+            if not accept == "Y" and not accept == "YES":
+                print("Abort")
                 quit()
-        subprocess.check_output(['git', 'pull', '--force', 'origin', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
-        print(f"Git get_branch and checkout {source}")
+        result = subprocess.check_output(['git', 'pull', '--force', 'origin', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
+        subprocess.check_output(['git', 'branch', f'--set-upstream-to=origin/{str(branch_name)}', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
+        print(f"OUTPUT : {result}")
     except subprocess.CalledProcessError as e:
         print(f"Git get_branch and checkout ERROR : {e.output.splitlines()[0]}")
 
