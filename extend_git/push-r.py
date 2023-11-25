3cf15f9613b5b877a39c663b75e11c0b30488768
Serem Titus(SeremTitus) <seremtitusdev@gmail.com>
2023-11-25 03:58:13 +0300

    add push

---
+++ extend_git/push-r.py
@@ -0,0 +1,31 @@
+import os
+import subprocess
+from queue import Queue
+
+def git_push(folder_path):
+    try:
+        subprocess.check_output(['git', 'push'], cwd=folder_path, stderr=subprocess.STDOUT, universal_newlines=True)
+        print(f"Git push successful in {folder_path}")
+    except subprocess.CalledProcessError as e:
+        print(f"Error in git push for {folder_path} : {e.output.splitlines()[0]}")
+
+def process_folder(folder_path, queue):
+    if os.path.exists(os.path.join(folder_path, '.git')):
+        git_push(folder_path)
+    else:
+        try:
+            subfolders = [f.path for f in os.scandir(folder_path) if f.is_dir()]
+            for subfolder in subfolders:
+                queue.put(subfolder)
+        except:
+            print(f"Access is denied: {folder_path}")
+
+
+starting_folder = os.getcwd()
+
+folder_queue = Queue()
+folder_queue.put(starting_folder)
+
+while not folder_queue.empty():
+    current_folder = folder_queue.get()
+    process_folder(current_folder, folder_queue)
