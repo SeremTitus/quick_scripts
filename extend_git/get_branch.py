875a9fdd2268e5fe67f5ee7eedd59a746db4ea22
Serem Titus(SeremTitus) <seremtitusdev@gmail.com>
2023-11-25 04:28:16 +0300

    safe fall back

--- extend_git/get_branch.py
+++ extend_git/get_branch.py
@@ -4,7 +4,13 @@ import subprocess
 
 def get_branch(branch_name):
     try:
-        subprocess.check_output(['git', 'checkout','-b', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
+        try:
+            subprocess.check_output(['git', 'checkout','-b', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
+        except subprocess.CalledProcessError as e:
+            print(f"Branch may exist ERROR : {e.output.splitlines()[0]}")
+            accept = input("branch will be rewriten,Y or YES to Continue anyway: ")
+            if not accept == "Y" or not accept == "YES":
+                quit()
         subprocess.check_output(['git', 'pull', '--force', 'origin', str(branch_name)], cwd=os.getcwd(), stderr=subprocess.STDOUT, universal_newlines=True)
         print(f"Git get_branch and checkout {source}")
     except subprocess.CalledProcessError as e:
