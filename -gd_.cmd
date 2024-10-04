71fd5d4afaa670115fdc169b8598137a5aa50f6d
Serem Titus <seremtitusdev@gmail.com>
2024-10-04 19:45:50 +0300

    update

--- -gd_.cmd
+++ -gd_.cmd
@@ -4,20 +4,26 @@ for /l %%i in (1,1,1000000) do (
     cls
     cd "C:\Users\Serem\Development\c\godot"
 
-    -gd_bd
-    
+    -gd_bd 
 
     if exist "%TEMP%\build_godot_result.txt" (
-        echo Build was successful
+        cls
         -gd_bd_run
         if exist "%TEMP%\build_godot_result.txt" (
-            echo good
+            cls
+            -gd_bd_test_gd
+            if exist "%TEMP%\build_godot_result.txt" (
+                cls
+            ) else (
+                powershell -c "[console]::Beep(1000, 1000)"
+                echo Godot crash -- any key to continue
+                pause
+            )
         ) else (
             powershell -c "[console]::Beep(1000, 1000)"
             echo Godot crash -- any key to continue
             pause
         )
-        -gd_bd_test_gd
     ) else (
         powershell -c "[console]::Beep(1000, 1000)"
         echo Build failed -- any key to continue
--- -gd_bd_test_gd.cmd
+++ -gd_bd_test_gd.cmd
@@ -1,2 +1,8 @@
 @echo off
 "C:\Users\Serem\Development\c\godot\bin\godot.windows.editor.dev.x86_64.exe" --test -ts="*[GDScript]*"
+if %ERRORLEVEL% EQU 0 (
+    echo Command was successful > "%TEMP%\build_godot_result.txt"
+) else (
+    echo Command failed with error level %ERRORLEVEL% > "%TEMP%\build_godot_result.txt"
+    del "%TEMP%\build_godot_result.txt"
+)
